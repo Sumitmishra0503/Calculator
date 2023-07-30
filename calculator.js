2ec67721abf9420ce3330539fd687a3855f51a00
Sumitmishra0503 <122718896+Sumitmishra0503@users.noreply.github.com>
2023-07-30 12:29:57 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,22 +1,23 @@
-const one = document.getElementById('one');
-const two = document.getElementById('two');
-const three = document.getElementById('three');
-const four = document.getElementById('four');
-const five = document.getElementById('five');
-const six = document.getElementById('six');
-const seven = document.getElementById('seven');
-const eight = document.getElementById('eight');
-const nine = document.getElementById('nine');
-const zero = document.getElementById('zero');
-const dot = document.getElementById('dot');
-const plus = document.getElementById('plus');
-const minus = document.getElementById('minus');
-const multiply = document.getElementById('mul');
-const divide = document.getElementById('divide');
-const equal = document.getElementById('equal');
-const reset = document.getElementById('reset');
+const one = document.getElementById('button1');
+const two = document.getElementById('button2');
+const three = document.getElementById('button3');
+const four = document.getElementById('button4');
+const five = document.getElementById('button5');
+const six = document.getElementById('button6');
+const seven = document.getElementById('button7');
+const eight = document.getElementById('button8');
+const nine = document.getElementById('button9');
+const zero = document.getElementById('button0');
+const dot = document.getElementById('button.');
+const plus = document.getElementById('button+');
+const minus = document.getElementById('button-');
+const multiply = document.getElementById('button*');
+const divide = document.getElementById('button/');
+const equal = document.getElementById('buttonEnter')
+const reset = document.getElementById('buttonEscape');
 const Result = document.querySelector('.result');
 const Equation = document.querySelector('.equation');
+const Backspace = document.getElementById('buttonBackspace');
 
 
 let String = '';
@@ -93,30 +94,49 @@ reset.onclick = function(){
     Result.value = '0';
     Equation.value = '';
 }
-Result.value = '0';
-document.body.onkeydown = function(){
-    if(event.key !== '0' && event.key !== '1' && event.key !== '2' && event.key !== '3' && event.key !== '4' && event.key !== '5' && event.key !== '6' && event.key !== '7' && event.key !== '8' && event.key !== '9' && event.key !== '0' && event.key !== '.' && event.key !== '/' && event.key !== '+' && event.key !== '*' && event.key !== '-' && event.key !== 'Enter' && event.key !== 'Backspace' && event.key !== '(' && event.key !== ')'){
+Backspace.onclick = function(){
+    if(String === '0' || !String){
         return;
     }
+    String  = String.slice(0,-1);
+    Equation.value = String || ''; 
+}
+Result.value = '0';
+document.body.onkeydown = function(){
+    if(event.key !== '0' && event.key !== '1' && event.key !== '2' && event.key !== '3' && event.key !== '4' && event.key !== '5' && event.key !== '6' && event.key !== '7' && event.key !== '8' && event.key !== '9' && event.key !== '0' && event.key !== '.' && event.key !== '/' && event.key !== '+' && event.key !== '*' && event.key !== '-' && event.key !== 'Enter' && event.key !== 'Backspace' && event.key !== '(' && event.key !== ')' && event.key !== 'Escape'){
+        return;
+    }
+    else if(event.key === 'Escape'){
+        String = '';
+        Result.value = '0';
+        Equation.value = '';
+    }
     else if(event.key === 'Backspace'){
-        console.log(event.key);
         if(String === '0' || !String){
             return;
         }
         String  = String.slice(0,-1);
         Equation.value = String || ''; 
-        
+        document.getElementById(`button${event.key}`).style.backgroundColor = "white";
         return;
     }
     else if(event.key === 'Enter'){
-        console.log(event.key);
-        String = ((eval(String))).toFixed(2);
-        Result.value = String;
+
+        String = ((eval(String))).toFixed(2) ;
+        Result.value = String || '0';
+        
         String = '';
+        document.getElementById(`button${event.key}`).style.backgroundColor = "white";
         return;
     }
-        console.log(event.key);
+    else{
         String += event.key;
-        Equation.value = String;
+    }
 
+    Equation.value = String;
+    document.getElementById(`button${event.key}`).style.backgroundColor = "white";
+
+}
+document.body.onkeyup = function() {
+    document.getElementById(`button${event.key}`).style.backgroundColor = "rgb(183, 179, 179)";
 }
